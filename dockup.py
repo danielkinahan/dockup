a02e62868225be9d1cde77bc2a6dbc5addf1f862
Daniel Kinahan <danielkinahan@uvic.ca>
2022-04-04 13:01:00 -0700

    Added reading off ps, committing image and storing as tar file

--- dockup.py
+++ dockup.py
@@ -1,12 +1,32 @@
+import subprocess
+from datetime import date
 
 #Docker backup script
 
+backup_dir = "/home/daniel/.docker_backup_staging/"
+
+docker_ps = ["docker", "ps", "--format", '"{{.ID}} {{.Names}}"']
 #Reads docker ps command and creates a dict with container names and ID's
 
-command = 'docker ps  --format "{{.ID}} {{.Names}}"'
+container_list = subprocess.run(docker_ps, capture_output=True)
 
+for item in container_list.stdout.decode("utf-8").split("\n")[:-1]:
+    item = item[1:-1]
+    container_id, container_name = item.split(" ")
 
-#Removes any entry in dict that matches blacklist by key/name
+    backup_name = "backup" + str(date.today()) + container_name
+    docker_commit = ["docker", "commit", "-p", container_id, backup_name]
+    #commit_return = subprocess.run(docker_commit, capture_output=True)
+    print(" ".join(docker_commit))
+
+    backup_tar = backup_dir + backup_name + ".tar"
+    docker_save = ["docker", "save", "-o", backup_tar, backup_name]
+    print(" ".join(docker_save))
+    #save_return = subprocess.run(docker_save, capture_output=True)
+
+    docker_image_rm = ["docker", "image", "rm", backup_name]
+    #docker_image_rm_return = subprocess.run(docker_image_rm, capture_output=True)
+    print(" ".join(docker_image_rm))
 
 #Reads docker volume ls and compares white list to remove as well
 
